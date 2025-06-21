820cf524c56fe7d66380b1f132b58c4d08e08dff
Antience100 <190290455+Antience100@users.noreply.github.com>
2025-06-20 21:42:34 -0400

    using custom css reset by josh w comeau - setting up css variables for styling, learned how to use JS localstorage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import "./styles.css";
 import { listItemFactory } from "./modules/listItemFactory";
-
-const testItem = listItemFactory("Make tacos", "make tacos with homemade seasoning", "19/6/2025 10:00PM", "high", "incomplete");
-
-testItem.itemInfo();
+import { parse } from "date-fns";
